6a65c373e4867e5a76a9615898bd54cc5a44e11b
imLucyk <88808248+imLucyk@users.noreply.github.com>
2022-02-15 20:13:22 +0900

    [#3] markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div class="logo">
+          RED
+        </div>
+        <div>
+          <a href="./index.html" id="menu-a-home" class="active"><span class="material-icons-outlined">home</span></a>
+        </div>
+        <div>
+          <a href="./items.html" id="menu-a-items"><span class="material-icons-outlined">shopping_cart</span></a>
+        </div>
+        <div class="menu-groceries">
+          <a href="./groceries.html" id="menu-a-groceries"><span class="material-icons">kitchen</span></a>
+          <div><span id="menu-groceries-counter">1</span></div>
+        </div>
+        <div class="empty"></div>
+        <div>
+          <a href="#!" id="menu-a-account" onclick="accountToggle(); return false;">
+            <span class="material-icons-outlined">account_circle</span>
+            <ul class="account-menu">
+              <li>Guest</li>
+              <li>Login</li>
+              <li>Hello 홍길동!</li>
+              <li>Logout</li>
+            </ul>
+          </a>
+        </div>
       </header>
-    </div>
+      <div class="contents">
+        <article class="home">
+          <h1>RED</h1>
+          <h2>Welcome to Refrigerator Expiry Date</h2>
+        </article>
+      </div>
+      <footer>© 2021. RED Co. all rights reserved.</footer>
+    </>
   );
 }
 
